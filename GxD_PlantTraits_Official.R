7efd8fb5cfca87d4573d575d0d8dce9cef9e8361
bloodworthk <kathryn.bloodworth3@gmail.com>
2022-12-19 17:16:58 -0500

    Make new script for paired down code

---
+++ GxD_PlantTraits_Official.R
@@ -0,0 +1,33 @@
+##########################################################################################################
+#Project: Dissertation: Community Weighted Plant Traits in MGP for Drought x Grazing 
+
+#Coder: Kathryn Bloodworth
+
+#Created: 12/14/2021
+##########################################################################################################
+
+#### Load Libraries ####
+
+#install.packages("lme4")
+library(lme4)
+#install.packages("ggfortify")
+library(ggfortify)
+library(ggplot2)
+#install.packages("vi>>> /usr/bin/git pullsreg")
+library(visreg)
+library(grid)
+#install.packages("lattice")
+library(lattice)
+#install.packages("FD")
+library(FD)
+#install.packages("pliman")
+library(pliman)
+#install.packages("multcomp")
+library(multcomp)
+#install.packages("factoextra")
+library(factoextra)
+#install.packages("emmeans")
+library(emmeans)
+#install.packages("PerformanceAnalytics")
+library(PerformanceAnalytics)
+library(tidyverse) 
